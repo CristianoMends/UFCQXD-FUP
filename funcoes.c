4a7e4d868daa0c757d610412017b2ec0772209ac
Rafaelleit3 <costtaraphael@gmail.com>
2023-06-25 10:33:43 -0300

    nova att

--- funcoes.c
+++ funcoes.c
@@ -1,10 +1,12 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "funcoes.h"
 #include "item.h"
 
 
 Produto produtos[MAX_PRODUTOS];		//vetor de produtos
+Produto guardarVendas[MAX_PRODUTOS]; //vetor de vendas
 
 void menu(){
 	int op;
@@ -24,9 +26,9 @@ void menu(){
 		case 2:
 			vender();
 			break;
-		/*case 3:
-			vendidos();
-			break;*/
+		case 3:
+			historico();
+			break;
 		case 4:
 			pesquisar();
 			break;
@@ -42,7 +44,7 @@ void menu(){
 
 void cadastrar(){
 	
-	char nome[MAX_LETRAS];
+	char nome[MAX_PRODUTOS];
 	float preco;
 	int quantidade;
 	int op;
@@ -51,8 +53,11 @@ do
 		system("cls");
         printf("\n----------Cadastro de produtos----------\n");
 		printf("Nome do produto: ");
-		fgets(nome, sizeof(nome), stdin); 			
-		nome[strcspn(nome, "\n")] = '\0'; // Remove o '\n' da string produto
+		fgets(nome, sizeof(nome), stdin);
+
+		//nome[strcspn(nome, "\n")] = '\0'; // Remove o '\n' da string produto
+		//************ o codigo acima impede a busca do produto*******//
+
 		printf("Digite o preco: ");
 		scanf("%f", &preco);
 		getchar();
@@ -60,7 +65,7 @@ do
 		scanf("%d", &quantidade);
 		getchar();
 	
-		//---------------------------------------------- enviando os lançamentos para o vetor produtos
+		//---------------------------------------------- enviando os lancamentos para o vetor produtos
 		int i;
 		for (i = 0; i < MAX_PRODUTOS; ++i){
 			if (produtos[i].disponivel == 0){
@@ -72,7 +77,7 @@ do
 				break;
 			}
 		}
-		//---------------------------------------------- menu de opções que fica embaixo
+		//---------------------------------------------- menu de opcoess que fica embaixo
 		printf("\nSTATUS: [%d] item(s) de [%s] cadastrado(s) ao estoque!\n",quantidade,nome);
 		printf("\n1 - Cadastrar novo produto\n0 - Menu principal\n");
 		scanf("%d", &op);
@@ -80,7 +85,7 @@ do
 	} while (op != 0);
 }
 
-//---------------------------------------------------------------função vender
+//---------------------------------------------------------------funcao vender
 void vender(){
 
 	int codigo;
@@ -93,12 +98,12 @@ void vender(){
 	scanf("%d", &qtde);
 	--codigo;
 
-	if (qtde <= produtos[codigo].quantidade )           // se a qtde for menor ou igual do que estoque estao da pra vender
+	if (qtde <= produtos[codigo].quantidade )           // se a qtde for menor ou igual do que estoque entao da pra vender
 	{
 		produtos[codigo].quantidade -= qtde;
 
 		qVendas++;
-		//vendidos(codigo, qtde);						//Atribui no historico de vendas
+		vendidos(&codigo, &qtde);						//Atribui no historico de vendas
 		printf("\n Produto vendido!\n");
 	}
 	else printf("\nProduto sem estoque");
@@ -111,67 +116,57 @@ void vender(){
 
 }
 
-//Produto historico[qVendas]; //alocação dinamica aqui
+//----------------------------------------------------------funcao pra retornar o historico de vendas
+void vendidos(int *codigo, int *qtde){
 
-//----------------------------------------------------------função pra retornar o histórico de vendas
-/*void vendidos(){
+	for(int i = 0; i < MAX_PRODUTOS; i++){
 
-	char vendas[qVendas];
-	float precos;
-	int quantidade;
+		guardarVendas[i].nome = produtos[codigo].nome;
+		guardarVendas[i].preco = produtos[codigo].preco;
+		guardarVendas[i].qtde = qtde;
+		break;
+	}
 
-
-
-
-
-
-
-}*/
-
-
-
-
-
-
-
-/*void vendidos(){
+}
+//----------------------------------------------------------------------- mostrar historico
+void historico(){
 
 	system("cls");
-	float media;
-	printf("\nLISTA DE PRODUTOS VENIDOS\n");
-
-	for (int i = 0; i < MAX_PRODUTOS; ++i)
-	{
-		if (produtos[i].disponivel == 1)
-		{
-			media = 0;
-			media = produtos[i].precos[0] + produtos[i].precos[1] + produtos[i].precos[2] + produtos[i].precos[3];
-			media = media / 4;
-			if (media < 7.0)
-			{
-				printf("codigo: %d\n", i + 1);
-				printf("produto: %s\n", produtos[i].produto);
-				printf("1 Bim: %0.2f\n", produtos[i].precos[0]);
-				printf("2 Bim: %0.2f\n", produtos[i].precos[1]);
-				printf("3 Bim: %0.2f\n", produtos[i].precos[2]);
-				printf("4 Bim: %0.2f\n", produtos[i].precos[3]);
-				printf("\n------------------------\n");
-			}
+	printf("\nPRODUTOS VENDIDOS\n");
+	int op;
+	do{	
+		int i;
+		for (i = 0; i < MAX_PRODUTOS; ++i){
+	
+			printf("Codigo: %d\n", i + 1);
+			printf("Produto: %s\n", guardarVendas[i].nome);
+			printf("Preco: %0.2f\n", guardarVendas[i].preco);
+			printf("Quantidade: %d\n", guardarVendas[i].quantidade);
+			printf("\n------------------------\n");
 			
-		}	
-	}
-}*/
+		}
 
-//-----------------------------------------------------------------------funcção pra pesquisar um item no estoque
+		printf("\n0 - Sair\n");
+		scanf("%d", &op);
+	} while (op != 0);
+
+}
+
+
+
+//-----------------------------------------------------------------------funcao pra pesquisar um item no estoque
 void pesquisar(){
 	system("cls");
 	char produto[MAX_PRODUTOS];
 	int op;
 	do{
 		system("cls");
+		printf("\n----------Pesquisa de produtos----------\n");
+
 		printf("\n Digite nome do produto: ");
 		fgets(produto, sizeof(produto), stdin);
 		int i;
+
 		for (i = 0; i < MAX_PRODUTOS; ++i)
 		{
 			if (strstr(produtos[i].nome, produto) != NULL)
@@ -191,7 +186,7 @@ void pesquisar(){
 
 }
 
-//----------------------------------------------------------------------- função para verificar todo o estoque
+//----------------------------------------------------------------------- funcao para verificar todo o estoque
 void estoque(){
 	system("cls");
 	printf("\nLISTA DE PRODUTOS\n");
--- item.h
+++ item.h
@@ -2,7 +2,7 @@
 #define MAX_LETRAS 20
 
 typedef struct {	
-	char nome[MAX_LETRAS];		//vetor de strings que armazena os nomes dos produtos
+	char nome[MAX_PRODUTOS];		//vetor de strings que armazena os nomes dos produtos
 	float preco;
 	int quantidade;
 	int disponivel;
